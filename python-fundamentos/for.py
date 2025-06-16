71305c6781db80385c31406d47385285f5091bde
CarlosVds <carloseduardovds@gmail.com>
2025-06-16 09:12:55 -0300

    Calculator BMI

---
+++ python-fundamentos/for.py
@@ -0,0 +1,7 @@
+# for number in range(2, 7):
+#     print(number)
+
+lista = [1, 2, 5,78, 546]
+
+for number in lista:
+    print(f"{number} X 2 = {number * 2}")
---
+++ python-fundamentos/while.py
@@ -0,0 +1,5 @@
+contador = 1
+
+while contador < 10:
+    print('Imprime 10 vezes')
+    contador += 1
